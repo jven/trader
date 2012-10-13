040cf36178383843277a9c9723db54d8365fe7e6
Justin Venezuela <jven@mit.edu>
2012-10-13 19:51:18 -0400

    Committing TradeAlgo

--- src/main/java/com/novus/trader/TradeAlgo.java
+++ src/main/java/com/novus/trader/TradeAlgo.java
@@ -9,42 +9,64 @@ import java.util.*;
  * @author Justin Venezuela (jven@mit.edu)
  */
 public class TradeAlgo extends ManagedAlgo {
-	
-	/** The training data for your algorithm. Maps a security id to a list of prices. */
-  private final Map<String, List<Double>> trainingData;
 
-  /** The required constructor. */
+  private final Map<String, PriceHistory> hists;
+  private final Map<String, TransactionLog> logs;
+  
   public TradeAlgo(Map<String, List<Double>> data) {
-    trainingData = data;
+    // we ignore the training data
+    hists = new HashMap<String, PriceHistory>();
+    logs = new HashMap<String, TransactionLog>();
+    for (String secName : data.keySet()) {
+      hists.put(secName, new PriceHistory(secName));
+      logs.put(secName, new TransactionLog(secName));
+    }
   }
 
-  /** The name of your algorithm. */
   public String getName() {
     return "jven";
   }
 
-  /**
-   * Your algorithm implementation resides here. This method is called by the provided
-   * client when a market event occurs. Such an event results in a new state notification
-   * from the server, which includes your current balance, positions, and the current
-   * prices of the market securities.
-   */
   public Map<String, TradeRequest> onMarketUpdate(TraderState state) {
+    // my monies
     Double balance = state.balance();
+    // amount I have
     Map<String, Integer> positions = state.positions();
+    // current price
     Map<String, Double> securities = state.securities();
-
-    return null;
+    
+    // report prices and transactions
+    for (String secName : securities.keySet()) {
+      if (hists.containsKey(secName)) {
+        hists.get(secName).reportPrice(state.securities().get(secName));
+      }
+      if (logs.containsKey(secName)) {
+        logs.get(secName).reportMarketUpdate(state);
+      }
+    }
+    
+    // make trade requests
+    Map<String, TradeRequest> ans = new HashMap<String, TradeRequest>();
+    for (String secName : hists.keySet()) {
+      if (logs.containsKey(secName) && positions.containsKey(secName)
+          && securities.containsKey(secName)) {
+        PriceHistory hist = hists.get(secName);
+        double price = securities.get(secName);
+        // split balance evenly amongst various securities
+        int amtIHave = positions.get(secName);
+        int amtCanBuy = (int)(balance / (hists.size() * price));
+        double myVal = hist.getValuation();
+        double bidP = Math.min(myVal, (price + myVal) / 2);
+        double askP = myVal + 0.01;
+        int amtCanSell = logs.get(secName).getAmountCanSellAtPrice(askP);
+        TradeRequest req = makeTradeRequest(amtCanBuy, bidP, amtCanSell, askP);
+        ans.put(secName, req);
+        logs.get(secName).reportRequest(amtIHave, amtCanBuy, bidP, amtCanSell, askP);
+      }
+    }
+    return ans;
   }
 
-  /**
-   * Create a trade request.
-   *
-   * @param bidQuantity   The number of shares you wish to buy.
-   * @param bidPrice      The price per share you are willing to pay.
-   * @param askQuantity   The number of shares you wish to sell.
-   * @param askPrice      The price per share you require.
-   */
   public TradeRequest makeTradeRequest(int bidQuantity, double bidPrice, int askQuantity,
       double askPrice) {
     return new TradeRequest(bidQuantity, bidPrice, askQuantity, askPrice);
